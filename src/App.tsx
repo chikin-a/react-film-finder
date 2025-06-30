a634e0a1da232a8733e9d683c337d3182ac31a8c
Anton Chikin <chikin.a.dev@gmail.com>
2025-07-01 00:07:33 +0300

    feat: implement basic routing in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router'
 import './App.scss'
+import { Home } from './pages/Home/Home'
 
 function App() {
-  return <></>
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='*' element={<h1>404 Not Found</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
 
 export default App
